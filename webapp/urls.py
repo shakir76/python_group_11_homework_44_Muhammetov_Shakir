ac72204092e0fc0a5be4654c5911c546c7eda7f0
shakir <shakirmuhammetov@gmail.com>
2022-06-21 18:14:01 +0600

    end commit

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from webapp.views import form_vie
+from webapp.views import form_vie, history
 
 urlpatterns = [
     path('', form_vie),
+    path('history/', history),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -2,24 +2,34 @@ from django.shortcuts import render
 
 from webapp.check_value import Check, ErrorValueRange, ErrorValueSet, ErrorValueDeficiency
 
+STEP = []
+MESSAGE = []
+WALK = 0
+
 
 def form_vie(request):
     secret_nums = [5, 1, 2, 9]
+
     if request.method == 'GET':
         return render(request, "index.html")
     else:
-        print(request.POST)
         value = request.POST.get("numbers")
         try:
             number_int = Check.check_int(value)
             Check.check_values(number_int)
             Check.check_set(number_int)
             Check.check_num(number_int, secret_nums)
+            global WALK
+            WALK += 1
+            global STEP
+            STEP.append(WALK)
             bulls, cows = Check.sum_bulls(number_int, secret_nums)
             if bulls == 4:
                 number_int = "You got it right!"
             else:
                 number_int = f"Bulls:{bulls} Cows:{cows}"
+            global MESSAGE
+            MESSAGE.append(number_int)
         except ValueError:
             number_int = "Error value not int"
         except ErrorValueRange:
@@ -33,5 +43,14 @@ def form_vie(request):
         }
         return render(request, "index.html", context)
 
+
+def history(request):
+    global STEP
+    global MESSAGE
+    context = {
+        'step': STEP,
+        'message': MESSAGE
+    }
+    return render(request, "history.html", context)
 
 # Create your views here.
